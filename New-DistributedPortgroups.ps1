9ebb3f6a4716680e5002c812e3cd9061826f52bc
mark-hargreaves-wei <mark.hargreaves@wei.com>
2026-07-17 12:30:58 -0400

    Initial Commit

---
+++ New-DistributedPortgroups.ps1
@@ -0,0 +1,21 @@
+$vcenter = Read-host "What is the FQDN of your vCenter?"
+$creds = Get-Credential
+
+Connect-VIServer $vcenter -Credential $creds
+
+$csv = Import-Csv "C:\users\mhargreaves\Downloads\vdsportgroups.csv"
+
+
+# Loop through and deploy directly to VDS
+$csv | ForEach-Object {
+    $vds = Get-VDSwitch -Name $_.vdsName -ErrorAction SilentlyContinue
+    if (-not $vds) { Write-Warning "Switch $($_.vdsName) not found"; return }
+
+    # Inline check and port group creation
+    if (Get-VDPortgroup -VDSwitch $vds -Name $_.PortGroup -ErrorAction SilentlyContinue) {
+        Write-Host "Skipped: Portgroup $($_.PortGroup) already exists." -ForegroundColor Yellow
+    } else {
+        New-VDPortgroup -Name $_.PortGroup -VDSwitch $vds -VlanId ($_.VlanID) | Out-Null
+        Write-Host "Created: $($_.PortGroup) on $($_.vdsName)" -ForegroundColor Green
+    }
+}
---
+++ New-StandardPortgroup.ps1
@@ -0,0 +1,33 @@
+$vcenter = Read-host "What is the FQDN of your vCenter?"
+$creds = Get-Credential
+
+Connect-VIServer $vcenter -Credential $creds
+
+$cluster = Read-host "What is the name of your cluster?"
+$csv = Import-Csv "C:\users\mhargreaves\Downloads\portgroups.csv"
+$VMhosts = Get-Cluster -Name $cluster | Get-VMHost
+
+#Group portgroups by switch object.  Prevents errors for Switch already created
+$csvSwitchName = $csv | Group-Object -Property vSwitchName
+
+
+#Looping through hosts in the cluster
+foreach ($VMhost in $VMhosts) {
+
+    #create standard switches
+    foreach ($csvSwitch in $csvSwitchName) {
+
+        $vss = $csvSwitch.name
+
+        $vSwitch = New-VirtualSwitch -VMHost $VMhost -Name $vss
+        Write-Host "  [+] Created vSwitch: $vss" -ForegroundColor Green
+            #create portgroups on standard switch
+            foreach ($row in $csvSwitch.Group) {
+                $pg = $row.PortGroup
+                $vlan = $row.VlanID
+            
+                New-VirtualPortGroup -VirtualSwitch $vSwitch -Name $pg -VLanId $vlan
+                Write-Host "    -> Created Port Group: $pg (VLAN $vlan)" -ForegroundColor Green
+            }
+    }
+}
